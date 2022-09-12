6b0c4a4063f957ae1d0d735235f6545487b847a8
Oliver Lenehan <55334808+sunsetkookaburra@users.noreply.github.com>
2022-09-12 11:46:16 +1000

    add shebang to example and add example script

--- example.ts
+++ example.ts
@@ -1,17 +1,3 @@
-# Bit Bin
-
-> Garbage In, Garbage Out!
-
-Binary data encoding and decoding in a convenient TypeScript library.
-
-## Available Data Types
-
-`Uint8`, `Uint16`, `Uint32`, `BigUint64`, `Int8`, `Int16`, `Int32`, `BigInt64`,
-`Bytes`, `Struct`, `Tuple`, `Vec`, `Utf8`
-
-## Example
-
-```ts
 #!/usr/bin/env -S deno run -r
 
 import { writeFull } from "https://deno.land/x/bitbin/mod.ts";
@@ -28,11 +14,3 @@ const size = await Uint8.readFrom(buf);
 const text = await Utf8(size).readFrom(buf);
 
 console.log(text);
-```
-
-## License
-
-Available under the [Mozilla Public License v2.0](./LICENSE).
-
-Use of Deno Standard Library utilities (`Buffer`) under
-[MIT License](https://github.com/denoland/deno_std/blob/main/LICENSE).
