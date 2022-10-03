3877d51a6f3ce4e4a653b85d9130cb515f2cbd45
Oliver Lenehan <55334808+sunsetkookaburra@users.noreply.github.com>
2022-10-04 07:48:59 +1100

    first cstring impl

---
+++ formats/cstring.ts
@@ -0,0 +1,27 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ * Copyright (C) Oliver Lenehan (sunsetkookaburra), 2022 */
+
+import { Codec, write, Buffer } from "../mod.ts";
+import { BytesRef } from "./mod.ts";
+
+/** Encodes and decodes UTF-8 null terminated strings. */
+export const CString: Codec<string> = {
+  label: "CString",
+  writeTo: async (sink, value) => {
+    const data = new TextEncoder().encode(value+"\0");
+    await write(sink, data);
+  },
+  readFrom: async (source) => {
+    const buf = new Buffer();
+    const cdc = BytesRef(1, "CString::readFrom");
+    let ch = await cdc.readFrom(source);
+    await write(buf, ch);
+    while (ch[0] != 0) {
+      ch = await cdc.readFrom(source);
+      await write(buf, ch);
+    }
+    return new TextDecoder().decode(buf.bytes()).slice(0, -1);
+  },
+};
