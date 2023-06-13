65e0dcd1bcbc8178a656102ab954135e143dc789
Oliver Lenehan <oli@sunsetkookaburra.xyz>
2023-06-13 21:20:34 +1000

    feat: pipeline stream

--- streams/pipe.ts
+++ streams/pipe.ts
@@ -3,36 +3,31 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  * Copyright (C) Oliver Lenehan (sunsetkookaburra), 2023 */
 
-type Tuple<T = unknown> = { [K in keyof T]: T[K] } & unknown[];
-// type Tuple = [{ [K in keyof T]: T[K] } & unknown[]];
-type Head<T extends Tuple> = T extends [infer U, ...unknown[]] ? U : never;
-type Tail<T extends Tuple> = T extends [...unknown[], infer U] ? U : never;
-export type Fn<Args extends Tuple = Tuple, Ret = unknown> = (...args: Args) => Ret;
-export type Args<F extends Fn> = F extends Fn<infer A, unknown> ? A : never;
-export type Ret<F extends Fn> = F extends Fn<Tuple, infer R> ? R : never;
+type WriteType<T> = T extends [TransformStream<infer I>, ...unknown[]] ? I : never;
+type ReadType<T> = T extends [...unknown[], TransformStream<unknown, infer O>] ? O : never;
 
-type Pipeline<I, T> = T extends [TransformStream<I, infer O>, ...infer Tail extends TransformStream[]] ? [TransformStream<I, O>, ...Pipeline<O, Tail>] : [];
+/** A pipe-checker that validates (correctly re-writes) a theoretical pipeline */
+type Transforms<T>
+// Suppose some tuple 'T'
+= T extends [TransformStream<infer I, infer M>, TransformStream<unknown, infer N>, ...infer Rest extends [...TransformStream[]]]
+  // To be correct, it should look like any of the following terminals/leafs ...
+  ? [TransformStream<I, M>, ...Transforms<[TransformStream<M, N>, ...Rest]>]
+  : T extends [TransformStream]
+    ? T
+    : [...TransformStream[]];
 
-type x = Pipeline<"A", [TransformStream<"A", "B">, TransformStream<"B", "D">]>;
+export class PipelineStream<T> implements TransformStream<WriteType<T>, ReadType<T>> {
+  #writable: WritableStream<WriteType<T>>;
+  #readable: ReadableStream<ReadType<T>>;
 
-export function pipe<I, A extends TransformStream[]>(...transforms: Pipeline<I, A>) {
+  constructor(...transforms: Transforms<T> & [TransformStream, ...TransformStream[]]) {
+    this.#writable = transforms[0].writable;
+    this.#readable = transforms[0].readable;
+    for (let i = 1; i < transforms.length; ++i) {
+      this.#readable = this.#readable.pipeThrough(transforms[i]);
+    }
+  }
 
+  get writable() { return this.#writable; }
+  get readable() { return this.#readable; }
 }
-
-// let y = pipe(new TransformStream<"A", "B">());
-
-// export function pipe<I, M, O, T extends Tuple<[TransformStream<I, unknown>, ...TransformStream[]] | [...TransformStream[], TransformStream<unknown, M>, TransformStream<M, unknown>, ...TransformStream[]] | [...TransformStream[], TransformStream<unknown,O>]>>(
-//   ...transforms: T
-// ): TransformStream<
-//   Head<T> extends TransformStream<infer I, unknown> ? I : never,
-//   Tail<T> extends TransformStream<unknown, infer O> ? O : never
-// > {
-//   const writable = transforms[0].writable;
-//   let readable = transforms[0].readable;
-//   for (let i = 1; i < transforms.length; ++i) {
-//     readable = readable.pipeThrough(transforms[i]);
-//   }
-//   return { writable, readable };
-// }
-
-// let x = pipe(new TransformStream<"A","B">(), new TransformStream<"E","D">())
