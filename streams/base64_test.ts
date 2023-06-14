4f29ca2a1f99ca853bfcf9b21c6819539eda5658
Oliver Lenehan <oli@sunsetkookaburra.xyz>
2023-06-14 20:56:36 +1000

    fix: base64 tests

--- streams/base64_test.ts
+++ streams/base64_test.ts
@@ -27,23 +27,30 @@ Deno.test({
   },
 });
 
+const ALPHA = new TextEncoder().encode(
+  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/="
+);
+
 Deno.test({
   name: "[streams/base64] Decode-Encode Fuzz",
   fn: async () => {
-    // for (let i = 0; i < 10; ++i) {
-      // const buf = new Uint8Array(i);
-      // crypto.getRandomValues(buf);
-      // const enc = new Uint8Array(
-      //   await new Response(
-      //     new Response(buf).body?.pipeThrough(new Base64EncoderStream()),
-      //   ).arrayBuffer(),
-      // );
-      // const dec = new Uint8Array(
-      //   await new Response(
-      //     new Response(enc).body?.pipeThrough(new Base64DecoderStream()),
-      //   ).arrayBuffer(),
-      // );
-      // assertEquals(dec, buf);
-    // }
+    for (let i = 2; i < 10; ++i) {
+      let str = "";
+      for (let j = 0; j < i; ++j) {
+        str += ALPHA[Math.trunc(Math.random() * 64)];
+      }
+      str = btoa(str);
+      const dec = new Uint8Array(
+        await new Response(
+          new Response(str).body?.pipeThrough(new Base64DecoderStream()),
+        ).arrayBuffer(),
+      );
+      const enc = new Uint8Array(
+        await new Response(
+          new Response(dec).body?.pipeThrough(new Base64EncoderStream()),
+        ).arrayBuffer(),
+      );
+      assertEquals(enc, new TextEncoder().encode(str));
+    }
   },
 });
